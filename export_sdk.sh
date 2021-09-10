bbbfb32a0284e8083f06d0c1da60d11e165f6448
Ken <sdken777@gmail.com>
2021-09-10 10:27:05 +0800

    新增3party文件夹

--- export_sdk.sh
+++ export_sdk.sh
@@ -12,11 +12,11 @@ cp -vf $CUR_DIR/lib64/spadas.lib $TARGET_DIR/lib64/
 
 mkdir -vp $TARGET_DIR/bin64
 cp -vf $CUR_DIR/bin64/spadas.dll $TARGET_DIR/bin64/
-cp -vf $CUR_DIR/bin64/libwinpthread-1.dll $TARGET_DIR/bin64/
+cp -vf $CUR_DIR/3party/windows/* $TARGET_DIR/bin64/
 
 mkdir -vp $TARGET_DIR/bin64/debug
 cp -vf $CUR_DIR/bin64/spadas.dll $TARGET_DIR/bin64/debug/
-cp -vf $CUR_DIR/bin64/libwinpthread-1.dll $TARGET_DIR/bin64/debug/
+cp -vf $CUR_DIR/3party/windows/* $TARGET_DIR/bin64/debug/
 
 mkdir -vp $TARGET_DIR/libx
 cp -vf $CUR_DIR/libx/libspadas.so $TARGET_DIR/libx/
