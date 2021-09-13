4cd1e40e43021d81da95189a62f4265a43e73691
Ken <sdken777@gmail.com>
2021-09-13 14:35:21 +0800

    export脚本dash兼容

--- export.sh
+++ export.sh
@@ -6,15 +6,15 @@ CUR_DATE=`date +%Y%m%d`
 
 EXPORT_DEVELOPER=y
 EXPORT_RUNTIME_DEBUG=y
-if [ "$1" == "" ]; then
+if [ "$1" = "" ]; then
     TARGET_DIR=~/Desktop/$CUR_DATE-$TARGET_NAME
     GEN_DESKTOP_ZIP=y
 else
     TARGET_DIR="$1"
-    if [ "$2" == "runtime-with-debug" ]; then
+    if [ "$2" = "runtime-with-debug" ]; then
         EXPORT_DEVELOPER=n
     fi
-    if [ "$2" == "runtime-without-debug" ]; then
+    if [ "$2" = "runtime-without-debug" ]; then
         EXPORT_DEVELOPER=n
         EXPORT_RUNTIME_DEBUG=n
     fi
@@ -30,7 +30,7 @@ cp -vf $CUR_DIR/binx/libspadas.so.$SPADAS_VERSION $TARGET_DIR/binx/
 mkdir -vp $TARGET_DIR/binxa
 cp -vf $CUR_DIR/binxa/libspadas.so.$SPADAS_VERSION $TARGET_DIR/binxa/
 
-if [ "$EXPORT_DEVELOPER" == "y" ]; then
+if [ "$EXPORT_DEVELOPER" = "y" ]; then
     mkdir -vp $TARGET_DIR/include
     cp -vf $CUR_DIR/include/spadas* $TARGET_DIR/include/
 
@@ -44,7 +44,7 @@ if [ "$EXPORT_DEVELOPER" == "y" ]; then
     cp -vf $CUR_DIR/libxa/libspadas.so $TARGET_DIR/libxa/
 fi
 
-if [ "$EXPORT_RUNTIME_DEBUG" == "y" ]; then
+if [ "$EXPORT_RUNTIME_DEBUG" = "y" ]; then
     mkdir -vp $TARGET_DIR/bin64/debug
     cp -vf $CUR_DIR/bin64/spadas.dll $TARGET_DIR/bin64/debug/
     cp -vf $CUR_DIR/3party/windows/* $TARGET_DIR/bin64/debug/
@@ -56,7 +56,7 @@ if [ "$EXPORT_RUNTIME_DEBUG" == "y" ]; then
     cp -vf $CUR_DIR/binxa/libspadas.so.$SPADAS_VERSION $TARGET_DIR/binxa/debug/
 fi
 
-if [ "$GEN_DESKTOP_ZIP" == "y" ]; then
+if [ "$GEN_DESKTOP_ZIP" = "y" ]; then
     cd ~/Desktop
     zip -r $CUR_DATE-$TARGET_NAME.zip ./$CUR_DATE-$TARGET_NAME/
     rm -r ./$CUR_DATE-$TARGET_NAME/
