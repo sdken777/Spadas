afe8410cc041b4039dd4237c7808459ee576f977
Ken <sdken777@gmail.com>
2021-09-14 14:15:44 +0800

    init_exe初始化脚本

--- init_exe.sh
+++ init_exe.sh
@@ -1,5 +1,10 @@
 CUR_DIR=$(dirname "$0")
 
+dos2unix $CUR_DIR/export.sh
+chmod -v +x $CUR_DIR/export.sh
+dos2unix $CUR_DIR/gen_apidoc.sh
+chmod -v +x $CUR_DIR/gen_apidoc.sh
+
 chmod -v +x $CUR_DIR/binx/test
 
 chmod -v +x $CUR_DIR/binxa/test
