bd3a0be7b15b94addaba27ee597e9caf282ece18
Ken <sdken777@gmail.com>
2022-03-14 15:21:02 +0800

    更新init_exe脚本

--- init_exe.sh
+++ init_exe.sh
@@ -2,13 +2,15 @@ CUR_DIR=$(dirname "$0")
 
 dos2unix "$CUR_DIR"/export.sh
 chmod -v +x "$CUR_DIR"/export.sh
+
+dos2unix "$CUR_DIR"/export_essential.sh
+chmod -v +x "$CUR_DIR"/export_essential.sh
+
 dos2unix "$CUR_DIR"/gen_apidoc.sh
 chmod -v +x "$CUR_DIR"/gen_apidoc.sh
 
 chmod -v +x "$CUR_DIR"/binx/test
-
 chmod -v +x "$CUR_DIR"/binxa/test
-
 chmod -v +x "$CUR_DIR"/binm/test
 
 echo "Done. (Spadas/init_exe.sh)"
