68f3829b63cd9bcac46c5515c49045ec80ec8d5f
Ken <sdken777@gmail.com>
2022-03-18 10:06:44 +0800

    8.2.6:MacOS使用C++11标准

--- include/spadas_define.h
+++ include/spadas_define.h
@@ -2,14 +2,14 @@
 #ifndef SPADAS_DEFINE_H
 #define SPADAS_DEFINE_H
 
-// 版本定义 // 新增IFilePluginV102
+// 版本定义 // MacOS使用C++11标准
 #define SPADAS_VERSION_MAJOR 8
 #define SPADAS_VERSION_MINOR 2
-#define SPADAS_VERSION_BUILD 5
+#define SPADAS_VERSION_BUILD 6
 
 /*! \mainpage
 * Spadas是支持Windows、Linux等操作系统的“一次编写到处编译”C++多功能类库。\n
-* 本文档对应Spadas版本：8.2.5\n
+* 本文档对应Spadas版本：8.2.6\n
 *
 * \n
 * \section top1 基本功能概述
