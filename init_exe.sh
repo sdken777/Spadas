de32c3ac36173a2c9580b2cf2462278bd0ae18a0
Ken <sdken777@gmail.com>
2023-01-06 15:21:02 +0800

    更新init_exe.sh

--- init_exe.sh
+++ init_exe.sh
@@ -3,9 +3,6 @@ CUR_DIR=$(dirname "$0")
 dos2unix "$CUR_DIR"/export.sh
 chmod -v +x "$CUR_DIR"/export.sh
 
-dos2unix "$CUR_DIR"/export_essential.sh
-chmod -v +x "$CUR_DIR"/export_essential.sh
-
 dos2unix "$CUR_DIR"/gen_apidoc.sh
 chmod -v +x "$CUR_DIR"/gen_apidoc.sh
 
