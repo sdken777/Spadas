aba5dda6c5c6791905ca6c20df0e43e8e7caa34e
Ken <sdken777@gmail.com>
2023-02-15 11:47:28 +0800

    8.6.3:新增VideoDataCodec::RAW16, Y16

--- Spadas/spadas_define.h
+++ Spadas/spadas_define.h
@@ -2,14 +2,14 @@
 #ifndef SPADAS_DEFINE_H
 #define SPADAS_DEFINE_H
 
-// 版本定义 // 新增Array::sort, sortAs。math::sort弃用(原算法可能排序失败)
+// 版本定义 // 新增VideoDataCodec::RAW16, Y16
 #define SPADAS_VERSION_MAJOR 8
 #define SPADAS_VERSION_MINOR 6
-#define SPADAS_VERSION_BUILD 2
+#define SPADAS_VERSION_BUILD 3
 
 /*! \mainpage
 * Spadas是支持Windows、Linux等操作系统的“一次编写到处编译”C++多功能类库。\n
-* 本文档对应Spadas版本：8.6.2\n
+* 本文档对应Spadas版本：8.6.3\n
 *
 * \n
 * \section top1 基本功能概述
