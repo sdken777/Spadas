34a5933783fed5eed325cf631a02511bf7617c11
Ken <sdken777@gmail.com>
2023-07-27 13:58:51 +0800

    导出文档不压缩

--- gen_apidoc.sh
+++ gen_apidoc.sh
@@ -4,9 +4,5 @@ cd "$CUR_DIR"/doc
 doxygen Doxyfile-Spadas
 cp -vf ./index.html ~/Desktop/D02006_SpadasDoc/
 
-cd ~/Desktop
-zip -r D02006_SpadasDoc.zip ./D02006_SpadasDoc/
-rm -r ./D02006_SpadasDoc/
-
 echo "Done. (Spadas/gen_apidoc.sh)"
 sleep 1
