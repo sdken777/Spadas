d5c6eaca3c65e4f0970253f9a334d6056635bd43
Ken <sdken777@gmail.com>
2023-09-15 09:59:11 +0800

    修正注释

--- Spadas/spadas_define.h
+++ Spadas/spadas_define.h
@@ -12,8 +12,8 @@
 /*! \mainpage
 * \~English Spadas is a "write once and compile everywhere" C++ multifunctional class library that supports Windows, Linux and other operating systems. \n
 * \~Chinese Spadas是支持Windows、Linux等操作系统的“一次编写到处编译”C++多功能类库。 \n
-* \~English This document corresponds to Spadas version: 8.10.5 \n
-* \~Chinese 本文档对应Spadas版本：8.10.5 \n
+* \~English This document corresponds to Spadas version: 8.10.6 \n
+* \~Chinese 本文档对应Spadas版本：8.10.6 \n
 * \~English The source code repository is: https://gitee.com/ken777/Spadas \n
 * \~Chinese 源码仓库位于： https://gitee.com/ken777/Spadas \n
 *
